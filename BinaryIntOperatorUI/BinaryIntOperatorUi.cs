b042e9ea81ed132248412d983b19c36f46540879
d-german <facmet107@gmail.com>
2018-09-17 19:06:34 -0500

    Finished for now

--- BinaryIntOperatorUI/BinaryIntOperatorUi.cs
+++ BinaryIntOperatorUI/BinaryIntOperatorUi.cs
@@ -3,14 +3,30 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ExampleClassLib;
 
 namespace BinaryIntOperatorUI
 {
     public partial class BinaryIntOperatorUi : Form
     {
+        private IBinaryIntOperator BuildBinaryIntOperator()
+        {
+            IBinaryIntOperator res;
+            if(ui_radioButtonAddition.Checked)
+            {
+                res = new IntAddition();
+            }
+            else
+            {
+                res = new IntSubtraction();
+            }
+
+            return res;
+        }
         public BinaryIntOperatorUi()
         {
             InitializeComponent();
@@ -18,18 +34,16 @@ namespace BinaryIntOperatorUI
 
         private void ui_radioButton_CheckedChanged(object sender, System.EventArgs e)
         {
-            if (ui_radioButtonAddition.Checked)
-            {
-                ui_labelOperator.Text = "+";
-            }
-            else
-            {
-                ui_labelOperator.Text = "-";
-            }
+            ui_labelOperator.Text = ui_radioButtonAddition.Checked ? "+" : "-";
         }
 
         private void button1_Click(object sender, System.EventArgs e)
         {
+            if(int.TryParse(ui_textBoxX.Text, out var x) && 
+               int.TryParse(ui_textBoxY.Text, out var y))
+            {
+                ui_textBoxResult.Text = BuildBinaryIntOperator().Operate(x, y).ToString();
+            }
 
         }
     }
