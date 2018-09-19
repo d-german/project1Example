f799a11f5b2fb4312c6c7444b95efb75f583d4de
d-german <facmet107@gmail.com>
2018-09-18 21:17:49 -0500

    Code cleanup

--- BinaryIntOperatorUI/BinaryIntOperatorUi.cs
+++ BinaryIntOperatorUI/BinaryIntOperatorUi.cs
@@ -1,11 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 using ExampleClassLib;
 
@@ -13,38 +6,32 @@ namespace BinaryIntOperatorUI
 {
     public partial class BinaryIntOperatorUi : Form
     {
-        private IBinaryIntOperator BuildBinaryIntOperator()
-        {
-            IBinaryIntOperator res;
-            if(ui_radioButtonAddition.Checked)
-            {
-                res = new IntAddition();
-            }
-            else
-            {
-                res = new IntSubtraction();
-            }
-
-            return res;
-        }
         public BinaryIntOperatorUi()
         {
             InitializeComponent();
         }
 
-        private void ui_radioButton_CheckedChanged(object sender, System.EventArgs e)
+        private IBinaryIntOperator BuildBinaryIntOperator()
+        {
+            IBinaryIntOperator res;
+            if (ui_radioButtonAddition.Checked)
+                res = new IntAddition();
+            else
+                res = new IntSubtraction();
+
+            return res;
+        }
+
+        private void ui_radioButton_CheckedChanged(object sender, EventArgs e)
         {
             ui_labelOperator.Text = ui_radioButtonAddition.Checked ? "+" : "-";
         }
 
-        private void button1_Click(object sender, System.EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            if(int.TryParse(ui_textBoxX.Text, out var x) && 
-               int.TryParse(ui_textBoxY.Text, out var y))
-            {
+            if (int.TryParse(ui_textBoxX.Text, out var x) &&
+                int.TryParse(ui_textBoxY.Text, out var y))
                 ui_textBoxResult.Text = BuildBinaryIntOperator().Operate(x, y).ToString();
-            }
-
         }
     }
 }
